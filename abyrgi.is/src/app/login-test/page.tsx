73b2f82703860750f978e417ab55da85de99dc24
aronfd <aron.frosti.davidsson@gmail.com>
2025-09-16 19:52:14 +0000

    Byrjaði að gera absolute import fyrir öll supabase skjöl en færi það frekar yfir í annað issue.

--- abyrgi.is/src/app/login-test/page.tsx
+++ abyrgi.is/src/app/login-test/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { createClient } from '../utils/supabase/client'
+import { createClient } from '@/utils/supabase/client'
 import { useState, useEffect } from 'react'
 
 export default function LoginTest() {
