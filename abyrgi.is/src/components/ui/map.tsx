c5d50dc217cd049e4f7470cfbc9561c392c44af2
Pétur <peturklettur@gmail.com>
2025-09-25 09:21:11 +0000

    tókk út marker

--- abyrgi.is/src/components/ui/map.tsx
+++ abyrgi.is/src/components/ui/map.tsx
@@ -14,9 +14,7 @@ const Map = () => {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }).addTo(map);
 
-        L.marker([64.1355, -21.8954]).addTo(map)
-            .bindPopup("This is Reykjavík!")
-            .openPopup();
+        
     }, []);
 
     return <div id="map" style={{ height: "500px", width: "100%" }}></div>;
